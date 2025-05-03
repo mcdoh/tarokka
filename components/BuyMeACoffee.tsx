af704019165661232b7f7efe2df5aa1915ecf973
Gavin McDonald <gavinmcdoh@gmail.com>
2025-05-03 16:30:40 -0400

    BuyMeACoffee glow

--- components/BuyMeACoffee.tsx
+++ components/BuyMeACoffee.tsx
@@ -6,7 +6,11 @@ type BuyMeACoffeeProps = {
 
 export default function BuyMeACoffee({ className = '' }: BuyMeACoffeeProps) {
 	return (
-		<a href="https://www.buymeacoffee.com/mcdoh" className={className} target="_blank">
+		<a
+			href="https://www.buymeacoffee.com/mcdoh"
+			className={`transition hover:drop-shadow-[0_0_3px_#ffd700] ${className}`}
+			target="_blank"
+		>
 			<img src="/img/bmc-button.svg" alt="Buy Me A Coffee" className="h-full" />
 		</a>
 	);
