25493671c598a8389f0a63965191d6df02491f00
Gavin McDonald <gavinmcdoh@gmail.com>
2025-05-17 18:10:53 -0400

    gussy up the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -16,13 +16,19 @@ export default function Home() {
 	};
 
 	return (
-		<main className="min-h-screen flex items-center justify-center bg-[url('/img/table3.png')] bg-cover bg-center">
-			<button
-				onClick={handleCreateGame}
-				className="bg-slate-800 hover:bg-slate-700 text-yellow-400 border border-yellow-500/25 hover:drop-shadow-[0_0_3px_rgba(255,215,0,0.5)] hover:text-yellow-300 text-lg px-6 py-3 rounded-lg shadow transition-all duration-250 cursor-pointer"
-			>
-				Create New Game
-			</button>
+		<main className="min-h-screen flex justify-center items-center text-yellow-400 bg-[url('/img/table3.png')] bg-cover bg-center">
+			<div className="flex flex-col items-center gap-8 text-center">
+				<h1 className="text-5xl font-bold text-center text-primary">Tarokka</h1>
+				<p className="text-l text-center w-[350px] m-auto">
+					Online Tarokka readings for <em>Dungeons & Dragons: Curse of Strahd</em>.
+				</p>
+				<button
+					onClick={handleCreateGame}
+					className="bg-slate-800 hover:bg-slate-700 border border-yellow-500/25 hover:drop-shadow-[0_0_3px_rgba(255,215,0,0.5)] hover:text-yellow-300 text-lg px-6 py-3 rounded-lg shadow transition-all duration-250 cursor-pointer"
+				>
+					Create New Game
+				</button>
+			</div>
 		</main>
 	);
 }
